166272909a8f5e5525271019bb9baf8f5b524acd
Tal Rozenman <tal.rozenman@gmail.com>
2024-05-17 22:39:08 +0100

    question 10

--- src/app/answers/10/page.tsx
+++ src/app/answers/10/page.tsx
@@ -2,9 +2,15 @@ export default function Answer() {
 	return (
 		<>
 			<h1 className="text-2xl font-bold mb-6">10. How could we improve or alter this familiarisation exercise? </h1>
-			<p>
+			<p className="mb-2">First of all, I will say I thoroughly enjoyed the exercise. Having previously heard of Vercel and Next.js, this was my first experience using them. I had a lot of fun working with the platform and was constantly surprised at all the thoughtful design nuances and intuitive features present. I found myself spending lots of time just playing around.</p>
 
-			</p>
+			<p className="mb-2">As for potential improvements to the exercise, I would suggest setting a design goal. While I understand this was certainly not the intent of the exercise, providing a reference layout would be helpful, so as not to waste too much time on the design aspect of the exercise.</p>
+
+			<p className="mb-2">Additionally, while I thought the questions were actually very well thought out and interesting to answer, I think it might be ambitious to properly answer all 10 in the space of 3 hours, while also setting up the platform and establishing a foundational project that is presentable.</p>
+
+			<p className="mb-2">All these are minor gripes at the end of the day, and I will say I think this was one of the most interesting technical exercises I have come across.</p>
+
+			<p className="mb-2">Thank you for the opportunity, and I'm looking forward to hearing from you :)</p>
 		</>
 	)
 }
--- src/app/answers/9/page.tsx
+++ src/app/answers/9/page.tsx
@@ -2,9 +2,22 @@ export default function Answer() {
 	return (
 		<>
 			<h1 className="text-2xl font-bold mb-6">9. What do you think is one of the most common problems which customers ask Vercel for help with? How would you help customers to overcome common problems, short-term and long-term?</h1>
-			<p>
+			<p>I think the most common problems Vercel customers encounter involve deployment errors or issues with serverless functions.</p>
 
-			</p>
+			<p className="mb-2">Some short-term approaches to overcoming these problems could include:</p>
+			<ul className="list-disc ml-6 mb-2">
+				<li className="mb-2">Diagnosing errors using Vercel's logs or error messages.</li>
+				<li className="mb-2">Ensure that <code>vercel.json</code> is correctly set up, with routes and builds properly defined.</li>
+				<li className="mb-2">Verify that all necessary environment variables are correctly added in the Vercel dashboard and match those in the <code>.env</code> file.</li>
+			</ul>
+
+			<p className="mb-2">Some long-term approaches to overcoming these problems could include:</p>
+			<ul className="list-disc ml-6 mb-2">
+				<li className="mb-2">Encourage customers to familiarize themselves with Vercel's documentation and guides to understand best practices and common pitfalls.</li>
+				<li className="mb-2">Guide customers to engage with the Vercel community, where they can find additional help and share experiences.</li>
+				<li className="mb-2">Lastly, I could offer training sessions or resources to help customers understand the nuances of deploying and managing serverless applications on Vercel.</li>
+			</ul>
+
 		</>
 	)
 }
